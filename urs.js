180bcc718a335590ae1cb6c9ee384838c043a5e3
mschmele <mschmele@gmail.com>
2017-09-07 12:25:01 -0400

    change listener to jquery, prevent default

--- urs.js
+++ urs.js
@@ -1,10 +1,13 @@
 urs = {
-  submit: function() {
-    let response = $.post({url: "https://urs.earthdata.nasa.gov/login",
-                           crossDomain: true,
-                           data: {username: $('#urs-username').val(),
-                                  password: $('#urs-password').val()}});
+  init: function() {
+    $('#urs-login').on('submit', function(event) {
+      event.preventDefault();
+      let response = $.post({url: "https://urs.earthdata.nasa.gov/login",
+                             crossDomain: true,
+                             data: {username: $('#urs-username').val(),
+                                    password: $('#urs-password').val()}});
 
-    console.log(response);
+      console.log(response);
+    });
   }
 }
